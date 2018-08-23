a72fe86768a8f16f4233d8aea60f900fd72ad29a
JPlukarski <jwplukarski@gmail.com>
2018-08-23 14:37:33 -0500

    more activities

---
+++ week-5/day-4/Activities/13-BestThingsEver/Unsolved/bestthingsever.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+
+fs.readFile("best_things_ever.txt", "utf8", function (error, data) {
+    if (error) {
+        return console.log(error);
+    }
+    console.log(data.split(","));
+
+
+});
